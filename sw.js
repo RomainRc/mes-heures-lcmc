ba546c6cdb3da5dcbd5a4cb8acd4c43c2221dbbd
RomainRc <rince.romain@orange.fr>
2026-09-09 13:50:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lcmc-v6';
+const CACHE='lcmc-v7';
 const ASSETS=['./','./index.html','./manifest.json','./sw.js','./logo-transparent.png','./header-bg.jpg','./logo.png','./icon-192.png','./icon-512.png','./straw-texture.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
